7c123c24de31ff64c604face8c04afe3e2fb3cab
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-27 04:38:28 +0700

    feat: update contact page with metadata

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,11 @@
 import Contact from "@/components/Contact";
 import FormContact from "@/components/FormContact";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact",
+  description: "This page shows the contact form of Di Dan Nguyen.",
+}
 
 function Page() {
   return (
