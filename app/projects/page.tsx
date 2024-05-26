b81cf146e8f97255f02bf3d60113f1c64e6a55fe
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-27 04:37:29 +0700

    feat: add title page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,10 @@
 import Project from "@/components/Project";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Project",
+  description: "This is a project page of Di Dan Nguyen where you can view all the projects that I have worked on."
+}
 
 function Page() {
   return <Project />;
