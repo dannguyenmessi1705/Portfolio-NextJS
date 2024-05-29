11de167a91b463f8dacba7a83112fc6e99b5bbdb
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-30 01:09:05 +0700

    feat: add error page

---
+++ app/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+export default function Error({ error, reset }: { error: any; reset: any }) {
+  return (
+    <main className="flex flex-col items-center justify-center gap-6">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+      <p className="text-lg">{error.message}</p>
+
+      <button
+        className="inline-block bg-accent-700 px-6 py-3 text-lg text-primary-50 rounded-sm hover:bg-accent-600 transition-colors"
+        onClick={reset}
+      >
+        Try again
+      </button>
+    </main>
+  );
+}
