fd0900282f9b218df298e824a2a9337912f1772d
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-09 02:48:59 +0700

    refractor: put code to page

--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,9 +1,38 @@
-import Homepage from "@/components/blog/new/Homepage";
+"use client";
 
-export default function page() {
+// import dynamic from "next/dynamic";
+import { useState } from "react";
+import MDEditor from "@uiw/react-md-editor";
+
+// const MDEditor = dynamic(
+//   () => import("@uiw/react-md-editor").then((mod) => mod.default),
+//   { ssr: false },
+// );
+
+// const EditerMarkdown = dynamic(
+//   () =>
+//     import("@uiw/react-md-editor").then((mod) => {
+//       return mod.default.Markdown;
+//     }),
+//   { ssr: false },
+// );
+
+// const Markdown = dynamic(
+//   () => import("@uiw/react-markdown-preview").then((mod) => mod.default),
+//   { ssr: false },
+// );
+
+export default function Page() {
+  const [value, setValue] = useState<string>("Let's write an article!");
   return (
-    <div>
-      <Homepage />
+    <div className="container mx-auto">
+      <MDEditor
+        value={value}
+        onChange={(newValue: string = "") => setValue(newValue)}
+        fullscreen={false}
+        preview="edit"
+        className=""
+      />
     </div>
   );
 }
