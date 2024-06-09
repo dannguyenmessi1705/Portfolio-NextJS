02d1267955af4fafaf3584692fe7c78bdcc1f4cb
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-09 18:27:31 +0700

    refractor code

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,4 +1,8 @@
-import AboutMe from "@/components/about/AboutMe";
+import Biogrophy from "@/components/about/Biogrophy";
+import Education from "@/components/about/Education";
+import Experience from "@/components/about/Experience";
+import Skillss from "@/components/about/Skills";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -11,7 +15,39 @@ export const metadata: Metadata = {
 function Page() {
   return (
     <section className="h-full w-full">
-      <AboutMe />
+      <div className="flex min-h-[70vh] items-center justify-center py-12 xl:py-0">
+        <div className="container mx-auto">
+          <Tabs
+            defaultValue="biogrophy"
+            className="flex flex-col gap-16 xl:flex-row"
+          >
+            <TabsList className="mx-auto flex w-full max-w-[380px] flex-col gap-6 xl:mx-0">
+              <TabsTrigger value="biogrophy">Biogrophy</TabsTrigger>
+              <TabsTrigger value="experience">Experience</TabsTrigger>
+              <TabsTrigger value="education">Education</TabsTrigger>
+              <TabsTrigger value="skills">Skills</TabsTrigger>
+            </TabsList>
+
+            <div className="min-h-[70vh] w-full">
+              <TabsContent
+                value="biogrophy"
+                className="w-full text-center xl:text-left"
+              >
+                <Biogrophy />
+              </TabsContent>
+              <TabsContent value="experience" className="h-full w-full">
+                <Experience />
+              </TabsContent>
+              <TabsContent value="education" className="h-full w-full">
+                <Education />
+              </TabsContent>
+              <TabsContent value="skills" className="h-full w-full">
+                <Skillss />
+              </TabsContent>
+            </div>
+          </Tabs>
+        </div>
+      </div>
     </section>
   );
 }
