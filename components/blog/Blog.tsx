73d53c680c413827cdbb9c3f01545b03e37250e3
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-11 03:40:05 +0700

    feat: create next link to blog

--- components/blog/Blog.tsx
+++ components/blog/Blog.tsx
@@ -6,6 +6,8 @@ import { type Blog } from "@/lib/data";
 import { useState } from "react";
 import Image from "next/image";
 import noImage from "@/public/assets/no-image.svg";
+import ButtonScrollTop from "../ui/ButtonScrollTop";
+import Link from "next/link";
 
 export default function Blog({ blogs }: { blogs: Blog[] }) {
   return (
@@ -17,48 +19,52 @@ export default function Blog({ blogs }: { blogs: Blog[] }) {
       }}
       className="space-y-8"
     >
-      {blogs ? (
-        blogs.map((blog) => (
-          <li key={blog.id} className="flex items-start gap-4 border-b p-4">
-            <div className="flex-shrink-0">
-              <Image
-                src={blog.adminAvatar ? blog.adminAvatar : noImage}
-                alt="avatar"
-                width={40}
-                height={40}
-                className="rounded-full"
-              />
-            </div>
-            <div className="flex-1">
-              <div className="mb-1 text-sm text-primary-400">
-                {blog.adminName} - {blog.date.split("T")[0]}
+      <ul>
+        {blogs ? (
+          blogs.map((blog) => (
+            <li key={blog.id} className="flex items-start gap-4 border-b p-4">
+              <div className="flex-shrink-0">
+                <Image
+                  src={blog.adminAvatar ? blog.adminAvatar : noImage}
+                  alt="avatar"
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                />
               </div>
-              <h2 className="mb-2 text-xl font-bold text-white">
-                {blog.title}
-              </h2>
-              <p className="mb-2 text-accent-400">{blog.excerpt}</p>
-              <div className="mb-2 flex items-center space-x-2">
-                <span className="rounded bg-accent-600 px-2 py-1 text-white">
-                  #didanblog
-                </span>
+              <div className="flex-1">
+                <div className="mb-1 text-sm text-primary-400">
+                  {blog.adminName} - {blog.date.split("T")[0]}
+                </div>
+                <h2 className="mb-2 text-xl font-bold text-white">
+                  <Link href={`/blogs/${blog.id}`} prefetch={true} className="cursor-pointer">
+                    {blog.title}
+                  </Link>
+                </h2>
+                <p className="mb-2 text-accent-400">{blog.excerpt}</p>
+                <div className="mb-2 flex items-center space-x-2">
+                  <span className="rounded bg-accent-600 px-2 py-1 text-white">
+                    #didanblog
+                  </span>
+                </div>
               </div>
-            </div>
-            <div className="relative hidden aspect-video w-2/6 flex-shrink-0 md:block">
-              <Image
-                src={blog.coverImage ? blog.coverImage : noImage}
-                alt={blog.title}
-                fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                quality={75}
-                loading="lazy"
-                className="rounded-lg object-cover"
-              />
-            </div>
-          </li>
-        ))
-      ) : (
-        <NotFound message="There are no blogs here" />
-      )}
+              <div className="relative hidden aspect-video w-2/6 flex-shrink-0 md:block">
+                <Image
+                  src={blog.coverImage ? blog.coverImage : noImage}
+                  alt={blog.title}
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  quality={75}
+                  loading="lazy"
+                  className="rounded-lg object-cover"
+                />
+              </div>
+            </li>
+          ))
+        ) : (
+          <NotFound message="There are no blogs here" />
+        )}
+      </ul>
       {/* <div className="container mx-auto mt-[30px] grid grid-cols-[1fr] gap-[30px] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {blogs.map((blog, index) => {
           return <BlogCard key={index} blog={blog} />;
