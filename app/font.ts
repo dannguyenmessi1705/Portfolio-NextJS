ac63853cf937f4d3139fed41b1468cc3f7f0d002
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-10 21:21:37 +0700

    prefetch true for font

--- app/font.ts
+++ app/font.ts
@@ -3,5 +3,5 @@ export const jetbrains = JetBrains_Mono({
   subsets: ["latin", "vietnamese"],
   display: "swap",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  preload: false,
+  preload: true,
 });
