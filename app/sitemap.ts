04965e1392346d7b986ee53d66a0ff35d98cb01d
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-11 18:26:16 +0700

    add robots and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,33 @@
+import { Blog } from "@/lib/data";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?page=all`,
+  );
+  const blogs: Blog[] = await res.json();
+  const blogEntries: MetadataRoute.Sitemap = blogs.map(({ id, date }) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/blogs/${id}`,
+    lastModified: new Date(date),
+    priority: 0.8,
+  }));
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about-me`,
+      lastModified: new Date().toISOString(),
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/projects`,
+      lastModified: new Date().toISOString(),
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
+      lastModified: new Date().toISOString(),
+      priority: 0.8,
+    },
+    ...blogEntries,
+  ];
+}
