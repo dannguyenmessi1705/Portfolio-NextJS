60563aea3a3c3a6502c62788fac1b51d1310e510
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-11 17:10:11 +0700

    update: stop loading data when cant get more datas

--- components/blog/LoadMoreBlog.tsx
+++ components/blog/LoadMoreBlog.tsx
@@ -13,12 +13,17 @@ let page: number = 1;
 export default function LoadMoreBlog() {
   const [ref, inView] = useInView();
   const [blogs, setBlogs] = useState<Blog[] | []>([]);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     if (inView) {
       getAllBlogsNoRoute(page.toString()).then((res) => {
-        setBlogs([...blogs, ...res]);
-        page++;
+        if (res.length === 0) {
+          setHasMore(false);
+        } else {
+          setBlogs([...blogs, ...res]);
+          page++;
+        }
       });
     }
   }, [inView, blogs]);
@@ -30,11 +35,13 @@ export default function LoadMoreBlog() {
           <BlogRender key={blog.id} blog={blog} />
         ))}
       </ul>
-      <section className="mt-4">
-        <div ref={ref} className="flex justify-center items-center">
-          <Image src={spinner} alt="spinner" width={50} height={50} />
-        </div>
-      </section>
+      {hasMore && (
+        <section className="mt-4">
+          <div ref={ref} className="flex items-center justify-center">
+            <Image src={spinner} alt="spinner" width={50} height={50} />
+          </div>
+        </section>
+      )}
     </>
   );
 }
