b2cd22270518d9b860ec4d5728e1b44834462f31
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-11 14:07:09 +0700

    add more skeleton in Blog

--- components/blog/BlogSkeleton.tsx
+++ components/blog/BlogSkeleton.tsx
@@ -1,16 +1,26 @@
-"use client";
-import { motion } from "framer-motion";
 export default function BlogSkeleton() {
   return (
-    <div className="animate-pulse flex space-x-4 p-4 border-b">
-      <div className="h-10 w-10 rounded-full bg-gray-300"></div>
-      <div className="flex-1 py-1">
-        <div className="h-4 bg-gray-300 rounded w-1/4 mb-2"></div>
-        <div className="h-4 bg-gray-300 rounded w-1/2 mb-2"></div>
-        <div className="h-6 bg-gray-300 rounded w-full mb-2"></div>
-        <div className="h-4 bg-gray-300 rounded w-full mb-2"></div>
-        <div className="h-4 bg-gray-300 rounded w-1/3"></div>
+    <>
+      <div className="flex animate-pulse space-x-4 border-b p-4">
+        <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+        <div className="flex-1 py-1">
+          <div className="mb-2 h-4 w-1/4 rounded bg-gray-300"></div>
+          <div className="mb-2 h-4 w-1/2 rounded bg-gray-300"></div>
+          <div className="mb-2 h-6 w-full rounded bg-gray-300"></div>
+          <div className="mb-2 h-4 w-full rounded bg-gray-300"></div>
+          <div className="h-4 w-1/3 rounded bg-gray-300"></div>
+        </div>
       </div>
-    </div>
+      <div className="flex animate-pulse space-x-4 border-b p-4">
+        <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+        <div className="flex-1 py-1">
+          <div className="mb-2 h-4 w-1/4 rounded bg-gray-300"></div>
+          <div className="mb-2 h-4 w-1/2 rounded bg-gray-300"></div>
+          <div className="mb-2 h-6 w-full rounded bg-gray-300"></div>
+          <div className="mb-2 h-4 w-full rounded bg-gray-300"></div>
+          <div className="h-4 w-1/3 rounded bg-gray-300"></div>
+        </div>
+      </div>
+    </>
   );
 }
