c5ca6c7d5ca7f37bbdb57e2f9720731027a50928
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-09-29 23:57:54 +0700

    fix api github

--- lib/github.ts
+++ lib/github.ts
@@ -5,14 +5,24 @@ type Stats = {
 };
 
 export async function getCurrentStreaks(): Promise<Stats> {
-  const data = await fetch(
-    `${process.env.GITHUB_CURRENT_STREAK}/${process.env.GITHUB_USERNAME}` as string,
-    { cache: "no-store" }
-  );
-  const res = await data.json();
-  return {
-    longestStreak: res.longestStreak.days,
-    codeCommits: res.totalContributions,
-    currentStreaks: res.currentStreak.days,
+  let stats: Stats = {
+    longestStreak: 0,
+    codeCommits: 0,
+    currentStreaks: 0,
   };
+  try {
+    const data = await fetch(
+      `${process.env.GITHUB_CURRENT_STREAK}/${process.env.GITHUB_USERNAME}` as string,
+      { cache: "no-store" },
+    );
+    const res = await data.json();
+    stats.longestStreak = res.longestStreak.days;
+    stats.codeCommits = res.totalContributions;
+    stats.currentStreaks = res.currentStreak.days;
+  } catch (error) {
+    stats.longestStreak = 138;
+    stats.codeCommits = 0;
+    stats.currentStreaks = 0;
+  }
+  return stats;
 }
